902a076a60f71adb20a0b051a84b1dd784959fa8
Ian D. Scott <ian@iandouglasscott.com>
2016-03-20 14:25:33 -0700

    Blinking led

--- test.v
+++ test.v
@@ -2,32 +2,19 @@ module test
 (
     input button,
     input clk,
-    output led1,
+    output reg led1,
     output led2,
     output led3
 );
-    //assign led1 = button;
-    //assign led2 = ~button;
-    //assign led3 = clk;
 
-    /* reg */
     reg [32:0] counter;
-    //reg state;
 
-    /* assign */
-    //assign led1 = state;
     assign led2 = 1'b1;
     assign led3 = 1'b1;
 
-    /* always */
     always @ (posedge clk) begin
-        if (!button)
-        begin
-            counter <= counter + 1;
-            led1 <= counter[26]; // <------ data to change
-        end
-        else
-            state <= 1'b1;
+        counter <= counter + 1;
+        led1 <= counter[26];
     end
 
 endmodule
