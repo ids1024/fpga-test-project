d244db656b0f24b196c6e96bd736d2799eab495c
Ian D. Scott <ian@iandouglasscott.com>
2016-03-20 15:00:57 -0700

    Simpler(?) design

--- test.v
+++ test.v
@@ -2,19 +2,17 @@ module test
 (
     input button,
     input clk,
-    output reg led1,
+    output led1,
     output led2,
     output led3
 );
 
     reg [32:0] counter;
 
+    assign led1 = counter[26];
     assign led2 = 1'b1;
     assign led3 = 1'b1;
 
-    always @ (posedge clk) begin
-        counter <= counter + 1;
-        led1 <= counter[26];
-    end
+    always @ (posedge clk) counter <= counter + 1;
 
 endmodule
